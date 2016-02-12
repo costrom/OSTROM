e981f7a6f3a10a9b350659cc6cbcdf82d1f40e5d
costrom <chris@costrom.com>
2016-02-12 11:37:07 -0800

    Update usstdatm1976.h

--- usstdatm1976.h
+++ usstdatm1976.h
@@ -1,6 +1,6 @@
 #ifndef USSTDATM1976
 #define USSTDATM1976
 
-void usstdatm1976(double alt, double& T, double& p, double& rho);
+void usstdatm1976(double& h, double& T, double& p, double& rho, double& mu, double& a);
 
 #endif
