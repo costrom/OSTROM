795e41ca818bd50d6bb6618d439fbd3c04cd2ccc
costrom <chris@costrom.com>
2016-02-12 12:08:52 -0800

    Update atmos.cpp

--- atmos.cpp
+++ atmos.cpp
@@ -1,80 +1,27 @@
 #include "atmos.h"
-#include <cmath>
-#include <iostream>
-#include <thread>
-#include <vector>
+#include "atmprops.h"
+#include "stdatm1976.h"
 
 using namespace std;
 
-extern vector<double> Altitudes;
-extern vector<double> Temperatures;
-extern vector<double> Pressures;
-extern vector<double> Densities;
-extern const double gamma;
-extern const double R;
-static const double S = 110.4;
-static const double beta = 1.458e-6;
-
-extern thread th;
-extern bool t_done;
-
-static const unsigned int sz = 100001;
-
-void atmos(double& h, double& T, double& p, double& rho, double& mu, double& a)
+void atmos(int& model, atmprops& atmProperties)
 {
-	if (!t_done)
+	switch (model)
 	{
-		th.join();
-		t_done = true;
-		cout << "\nAtmos loaded!\n";
-	};
-
-	if (h > 1e6)
-	{
-		h = 1e6;
-	};
-
-	if (h < 0)
-	{
-		h = 0.0;
-	};
-
-	unsigned int qq = 0;
-
-	while (qq < (sz - 2) && Altitudes[qq] * 1000.0 < h)
-	{
-		qq++;
-	};
-
-	if (qq >= sz)
-	{
-		qq = sz - 1;
-	};
-
-	if (Altitudes[qq] == h)
-	{
-		T = Temperatures[qq];
-		p = Pressures[qq];
-		rho = Densities[qq];
+		case 1: // US STD ATM 1976
+			usstdatm1976(atmProperties);
+			break;
+		case 2: // MSISE
+			msise(atmProperties);
+			break;
+		case 3: // Jacchia
+			jacchia(atmProperties);)
+			break;
+		case 4: // GRAM
+			gram99(atmProperties);
+			break;
+		default:
+			break;
 	}
-	else
-	{
-		qq--;
-		const double hL = Altitudes[qq] * 1000.0;
-		const double hU = Altitudes[qq + 1] * 1000.0;
-		const double TL = Temperatures[qq];
-		const double TU = Temperatures[qq + 1];
-		const double pL = Pressures[qq];
-		const double pU = Pressures[qq + 1];
-		const double rhoL = Densities[qq];
-		const double rhoU = Densities[qq + 1];
-		const double ratio = (h - hL) / (hU - hL);
-
-		T = (TU - TL)*ratio + TL;
-		p = (pU - pL)*ratio + pL;
-		rho = (rhoU - rhoL)*ratio + rhoL;
-	};
-	mu = beta*pow(T, 1.5) / (T + S);
-	a = sqrt(gamma*R*T);
 	return;
 };
