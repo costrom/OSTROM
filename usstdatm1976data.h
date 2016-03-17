ef1faa5524d81112ae2b2c56852db2e5589feb1d
costrom <chris@costrom.com>
2016-03-17 10:53:42 -0700

    Update usstdatm1976data.h

--- usstdatm1976data.h
+++ usstdatm1976data.h
@@ -3,9 +3,9 @@
 
 #include <vector>
 
-extern const std::vector<double> Altitudes(100001);
-extern const std::vector<double> Temperatures(100001);
-extern const std::vector<double> Pressures(100001);
-extern const std::vector<double> Densities(100001);
+const static double Altitudes[];
+const static double Temperatures[];
+const static double Pressures[];
+const static double Densities[];
 
 #endif
